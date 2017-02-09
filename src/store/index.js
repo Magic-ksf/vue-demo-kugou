f14584ee085c3d3e573d31c0e58215f63372aa1b
Magic-ksf <Magic-ksf@users.noreply.github.com>
2017-02-09 16:35:18 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -95,7 +95,7 @@ const store = new Vuex.Store({
     getSong({commit,state}, hash){
       commit('toggleAudioLoadding', true);
       Vue.http.get(`http://cs003.m2828.com/apis/getKugouSong.php?hash=${hash}`).then(res=> {
-        var json_obj = JSON.parse(res.data);
+        var json_obj = res.data;
         var songUrl = json_obj.url,
           imgUrl = json_obj.imgUrl.split('{size}').join('100'),
           title = json_obj.songName,
